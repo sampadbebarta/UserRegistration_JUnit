17d42d9b0cbd45d299d60ac03c25fd5994edc64b
Sampad <bebartasampad@gmail.com>
2021-02-03 14:31:14 +0530

    Validate all details of User Registration

--- src/main/java/com/bridgelab/demo/UserValidation.java
+++ src/main/java/com/bridgelab/demo/UserValidation.java
@@ -1,57 +1,36 @@
 package com.bridgelab.demo;
 
 public class UserValidation {
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO USER REGISTRATION");
-    }
-    public static String validateFirstAndLastName(String fname) {
-        String firstNamePattern = "^[A-Z]{1}[a-zA-Z]{3,}$";
-        if (fname.matches(firstNamePattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public static String validateEmail(String email) {
-        String emailPattern = "^[a-zA-Z0-9]{3,}([+-._]{1}[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,3}([.]{1}[a-zA-Z]{2,3})?$";
-        if (email.matches(emailPattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public static String validateMobileNumber(String mobilenumber) {
-        String mobileNumberPattern = "^[1-9]{1,3}[ ][0-9]{10}$";
-        if (mobilenumber.matches(mobileNumberPattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public static String validateEightCharacterPassword(String password) {
-        String passwordRuleOnePattern = "^[a-zA-Z]{8,}";
-        if (password.matches(passwordRuleOnePattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public String validatePasswordWithAtLeastOneCapitalLetter(String password) {
-        String passwordRuleTwoPattern="^(?=.*[A-Z])[a-zA-Z]{8,}$";
-        if (password.matches(passwordRuleTwoPattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public static String validatePasswordWithAtLeastOneNumericNumber(String password) {
-        String passwordRuleThreePattern = "^(?=.*[A-Z])(?=.*[\\d])[a-zA-Z0-9]{8,}$";
-        if (password.matches(passwordRuleThreePattern)) {
-            return "Valid";
-        }
-        return "Invalid";
-    }
-    public String validatePasswordWithAtLeastOneSpecialCharacter(String password) {
-        String passwordRuleFourPattern= "^((?=[^\\W]*[\\W][^\\W]*$))(?=.*[A-Z])(?=.*[\\d])(?=.*[a-z])[A-za-z0-9\\W].{8,}$";
-        if (password.matches(passwordRuleFourPattern))
-        {
-            return "Valid";
-        }
-        return "Invalid";
-    }
+
+	public String AnalyseMood(String message) {
+		if(message.contains("Sad"))
+			return "SAD";
+		return "HAPPY";
+	}
+
+	public void PrintWelcomeMessage() {
+		System.out.println("Welcome to the User Validation Problem");
+	}
+
+	public boolean ValidateFirstName(String firstName) {
+		return firstName.matches("([A-Z][a-z]{2,})");
+	}
+
+	public boolean ValidateLastName(String lastName) {
+		return lastName.matches("([A-Z][a-z]{2,})");
+	}
+
+	public boolean ValidateEmail(String email) {
+		return email.matches("(^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}+(?:\\.[a-z]{2,}){0,1}$)");
+	}
+
+	public boolean ValidatePhoneNumber(String phoneNumber) {
+		return phoneNumber.matches("(([0-9]{2})?)[ ][0-9]{10}");
+	}
+
+	public boolean ValidatePassword(String password) {
+		return password.matches("((^(?=.*[A-Z]))(?=.*[0-9])(?=.*[a-z])(?=.*[@*&^%#-*+!]{1}).{8,}$)");
+	}
+
+
 }
--- src/test/java/com/bridgelab/demo/UserRegistrationTest.java
+++ src/test/java/com/bridgelab/demo/UserRegistrationTest.java
@@ -3,101 +3,144 @@ package com.bridgelab.demo;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UserRegistrationTest {
-    @Test
-    public void givenFirstName_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateFirstAndLastName("Sampad");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenFirstName_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateFirstAndLastName("sampad");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenLastName_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateFirstAndLastName("Rahul");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenLastName_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateFirstAndLastName("rahul");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenEmail_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateEmail("abc@yahoo.com");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenEmail_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateEmail("abc@%*.com");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenMobileNumber_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateMobileNumber("91 9437239407");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenMobileNumber_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateMobileNumber("916576576");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenPasswordStartWithEightCharacter_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateEightCharacterPassword("Workaholic");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenPasswordStartWithEightCharacter_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validateEightCharacterPassword("workaholic3452");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenPasswordContainsAtLeastOneUppperCaseCharacter_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneCapitalLetter("WorkAholic");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenPasswordContainsAtLeastOneUppperCaseCharacter_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneCapitalLetter("hackerwork");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenPasswordContainsAtLeastOneNumericCharacter_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneNumericNumber("WorkAholic2341");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenPasswordContainsAtLeastOneNumericCharacter_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneNumericNumber("unboundcodes");
-        Assert.assertEquals("Invalid",result);
-    }
-    @Test
-    public void givenPasswordContainsExactlyOneSpecialCharacter_WhenProper_ShouldReturnValid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneSpecialCharacter("@WorkAholic2341");
-        Assert.assertEquals("Valid",result);
-    }
-    @Test
-    public void givenPasswordContainsExactlyOneSpecialCharacter_WhenInproper_ShouldReturnInvalid() {
-        UserValidation userValidation = new UserValidation();
-        String result=userValidation.validatePasswordWithAtLeastOneSpecialCharacter("@#unboun@dcodes");
-        Assert.assertEquals("Invalid",result);
-    }
+public class UserValidationTest
+{
+
+	UserValidation userValidation = new UserValidation();
+
+	@Test
+	public void PrintWelcomeMessage() {
+		userValidation.PrintWelcomeMessage();
+	}
+
+	@Test
+	public void Given_Message_When_Sad_Should_Return_Sad() {
+		String mood = userValidation.AnalyseMood("This is a Sad Message");
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void Given_Message_When_Happy_Should_Return_Happy() {
+		String mood = userValidation.AnalyseMood("This is a Happy Message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void given_FirstName_CapitalLetter_return_true()
+	{
+		boolean result = userValidation.ValidateFirstName("Sampad");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_FirstName_withMixedAlphabets_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("SAmpad");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_FirstName_Alphanumeric_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("Sam6ad");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_LastName_CapitalLetter_return_true()
+	{
+		boolean result = userValidation.ValidateLastName("Rahul");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_LastName_withMixedAlphabets_return_false()
+	{
+		boolean result = userValidation.ValidateLastName("RAhul");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_LastName_Alphanumeric_return_false()
+	{
+		boolean result = userValidation.ValidateLastName("Ra6ul");
+		Assert.assertEquals( result, false );
+	}
+
+	@Test
+	public void given_Email_when_given_CountryDomain_return_true()
+	{
+		boolean result = userValidation.ValidateEmail("abc@abc.com.au");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_Email_when_emailStartswithDot_return_false()
+	{
+		boolean result = userValidation.ValidateEmail(".abc@gmail.com");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_Email_containsOptional_Special_Character_return_true()
+	{
+		boolean result = userValidation.ValidateEmail("abc@%*.com.com");
+		Assert.assertEquals( false, result );
+	}
+
+	@Test
+	public void given_PhoneNumber_with_valid_countryCode_return_true()
+	{
+		boolean result = userValidation.ValidatePhoneNumber("91 9348239407");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_PhoneNumber_with_No_countryCode_return_false()
+	{
+		boolean result = userValidation.ValidatePhoneNumber("9348239407");
+		Assert.assertFalse( result );
+	}
+	@Test
+	public void given_PhoneNumber_with_Invalid_countryCode_return_false()
+	{
+		boolean result = userValidation.ValidatePhoneNumber("091 9348239407");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_correct_return_true()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasHds98&");
+		Assert.assertTrue( result);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_incorrect_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasds98&");
+		Assert.assertFalse( result);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_more_than_one_Specialcharacter_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasds9&5%8&");
+		Assert.assertEquals( result, false);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_No_UpperCharacter_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasds9&5");
+		Assert.assertEquals( result, false);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_No_Digit_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zCsfkasds&");
+		Assert.assertEquals( result, false);
+	}
+
 }
